b4cf60e38e0fc8a77e2db0259079936cbb89071a
MuzamilWazir <wazirmuhammadmuzammil@gmail.com>
2025-05-23 11:14:19 +0500

    Remove Bug

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,25 +1,23 @@
-class ApiError extends Error{
-    constructor(
-        statusCode,
-        messasge = "Something Went Wrong",
-        errors = [],
-        statck=''
-    ) {
-        super(messasge)
-        this.statusCode = statusCode,
-            this.data = null,
-            this.message = messasge,
-            this.success = false,
-            this.errors = errors
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    messasge = "Something Went Wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(messasge);
+    (this.statusCode = statusCode),
+      (this.data = null),
+      (this.message = messasge),
+      (this.success = false),
+      (this.errors = errors);
 
-
-            if (statck) {
-                this.statck = statck
-        }
-            else {
-                Error.captureStackTrace(this,this.contructor)
-        }
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.contructor);
     }
+  }
 }
 
-export {ApiError}
+export { ApiError };
